2c2fc12837af0e75ffda99b484a2a4a55771a110
apppur <applepurplediandian@gmail.com>
2016-08-25 01:16:51 +0800

    move to cherrytools

--- Tools/robot/basecore.cpp
+++
@@ -1,8 +0,0 @@
-#include "basecore.h"
-
-unsigned int bit32_extract(unsigned int value, int pos, int size)
-{
-	//return (((value >> pos) & MASK(size)));
-	unsigned char shift = sizeof(int)* 8 - pos - size;
-	return size << shift >> pos + shift;
-}
