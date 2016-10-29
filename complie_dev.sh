bd0f028b3e36e3294153779539989bb648084b74
smartlean <smartlean1@gmail.com>
2016-10-29 18:30:49 +0800

    add compile xocde sh

---
+++ complie_dev.sh
@@ -0,0 +1,5 @@
+/usr/bin/security unlock-keychain -p Iamufo821
+/usr/bin/xcodebuild -project /Users/smartlean/Documents/majiang/xcode/Unity-iPhone.xcodeproj PROVISIONING_PROFILE=4bcf1f09-4967-4c6c-820c-487245c8ca13 -sdk iphoneos CODE_SIGN_IDENTITY="iPhone Developer: Shu-Ping Chou (8JMEFPBBGM)" GCC_GENERATE_DEBUGGING_SYMBOLS=YES DEBUG_INFORMATION_FORMAT=dwarf-with-dsym DWARF_DSYM_FILE_SHOULD_ACCOMPANY_PRODUCT=YES DEBUGGING_SYMBOLS=YES PRODUCT_NAME=hzmj -configuration Release 
+/usr/bin/xcrun -sdk iphoneos PackageApplication -v /Users/smartlean/Documents/majiang/xcode/build/Release-iphoneos/hzmj.app -o /Users/smartlean/Documents/majiang/xcode/build/hzmj.ipa
+
+mv /Users/smartlean/Documents/majiang/xcode/build/hzmj.ipa /Users/smartlean/Documents/majiang/xcode/build/hzmj_dev.ipa
