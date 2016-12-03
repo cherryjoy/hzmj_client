c5d0b95b545705c5b5a95622a891cf7f6a33a1aa
smartlean <smartlean1@gmail.com>
2016-12-03 19:21:27 +0800

    上传1.0.4 ios资源

---
+++ buildAndroidVersion.bat
@@ -0,0 +1,5 @@
+echo off
+
+DataGen.exe F:\MaJiang\hzmj_client\TragetAndroid F:\MaJiang\hzmj_client\DestAndroid\1.0.4
+
+pause.
---
+++ buildIOSVersion.bat
@@ -0,0 +1,5 @@
+echo off
+
+DataGen.exe F:\MaJiang\hzmj_client\TragetIOS F:\MaJiang\hzmj_client\DestIOS\1.0.4
+
+pause.
