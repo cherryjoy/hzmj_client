a049b73c2fa9af4df501a6fb0ea6a904976fe43d
smartlean <smartlean1@gmail.com>
2016-12-12 16:55:52 +0800

    修改android小包内容

--- Assets/Script/Editor/BuildAssetBundle.cs
+++ Assets/Script/Editor/BuildAssetBundle.cs
@@ -1052,10 +1052,10 @@ public class BuildAssetBundle : EditorWindow
     FileInfo[] GetSmallAssetBundles()
     {
         string[] assetbundlesBuffer ={"/music/ui/buttonclick_13",
-                                     "/ui/atlas/bit16/Emotion",
-                                     "/ui/atlas/bit32/baseui",
+                                     "/ui/atlas/bit32/base",
                                      "/ui/atlas/bit32/login",
                                      "/ui/fonts/msyh",
+                                     "/ui/prefab/UpdateControl",
                                      "/ui/prefab/toolsui/messagebox",
                                      "/assetbundles","/shader"};
 
