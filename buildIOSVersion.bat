ffb685faadfe216e6fb5c0c1bc96cc49bcb01932
smartlean <smartlean1@gmail.com>
2017-02-10 22:01:42 +0800

    * 版本升至1.3.0

--- buildIOSVersion.bat
+++ buildIOSVersion.bat
@@ -1,5 +1,5 @@
 echo off
 
-DataGen.exe F:\MaJiang\hzmj_client\TragetIOS F:\MaJiang\hzmj_client\DestIOS\1.2.6
+DataGen.exe F:\MaJiang\hzmj_client\TragetIOS F:\MaJiang\hzmj_client\DestIOS\1.3.0
 
 pause.
