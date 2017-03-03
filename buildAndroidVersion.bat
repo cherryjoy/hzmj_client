b027b81158c12aebef4304ec61eb5a0e599fcc54
smartlean <smartlean1@gmail.com>
2017-03-03 23:28:24 +0800

    + 上传1.4.1资源

--- buildAndroidVersion.bat
+++ buildAndroidVersion.bat
@@ -1,5 +1,5 @@
 echo off
 
-DataGen.exe F:\MaJiang\hzmj_client\TragetAndroid F:\MaJiang\hzmj_client\DestAndroid\1.3.0
+DataGen.exe F:\MaJiang\hzmj_client\TragetAndroid F:\MaJiang\hzmj_client\DestAndroid\1.4.1
 
 pause.
--- buildIOSVersion.bat
+++ buildIOSVersion.bat
@@ -1,5 +1,5 @@
 echo off
 
-DataGen.exe F:\MaJiang\hzmj_client\TragetIOS F:\MaJiang\hzmj_client\DestIOS\1.3.0
+DataGen.exe F:\MaJiang\hzmj_client\TragetIOS F:\MaJiang\hzmj_client\DestIOS\1.4.1
 
 pause.
